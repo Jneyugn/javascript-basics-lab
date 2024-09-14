98da9b18834b0aa218501a556df6b3c4f59b2fea
Jneyugn <j.nguyeen@hotmail.com>
2024-09-13 20:23:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+
+
+//Declare and Use Variables
+
+// Example: Declaring and using a variable
+let greeting = "Hello, World!";
+console.log(greeting); // This should print "Hello, World!" in the console
+
+var studentName; "Annie";
+studentName = "Jenny"
+//Variable 1
+var studentAge = 24
+//Variable 2
+console.log(studentName); 
+console.log(studentAge);
+let isEnrolled = true
+console.log(isEnrolled);
+//Variable 3
+
+
+// Demonstrate Data Types
+// String
+
+let myName = "Jenny"; 
+let myAge = 24;
+
+let lastName = "Nguyen";
+
+const person = new Object();
+person .age = 30;
+person .name = "Dor";
+console.log(person.name);
+//Object
+
+let isFinished = false
+console.log(isFinished);
+// Demonstrate Data Types - Boolean
+
+let distance = 222.2;
+console.log(distance);
+// Demonstrate Data Types - Number
+
+
+// Demonstrate Data Types - Array
+
+let colors = ['Purple', 'Grey', 'Black'];
+console.log(colors[0]); // Accesses the first element, output: 'Purple'
+colors.push('Pink'); // Adds a new element at the end
+console.log(colors); // Output: ['Purple', 'Grey', 'Black', 'Pink']
+colors.pop();
+console.log(colors);
+
+console.log(`hello ${person.name}. I like those colors`)
+
+console.log (`My name is ${myName} and I am a student learning js! My program cordinator is ${person.name}! I am ${myAge} years old! `) // Demonstrate Data Types - Object
+
+//Use Basic Operators
+
+//Arithmetic Operators:
+console.log(500 * 4); // Multiplication,
+console.log(4 / 2); // Division
+console.log(11 % 3); // Modulus (Remainder)
+
+console.log(15 + 5); // Addition
+console.log(10 - 8); // Subtraction
+
+//Comparison Operators:
+
+let x = 5; 
+let y = 10;
+console.log("10" == y); // Equality
+console.log("10" === 3); // Strict equality
+console.log(x != y); // Inequality
+console.log(x < y); // Less than
+console.log(x > y); // Greater than
+console.log(x <= y); // Less than or equal to
+console.log(x >= y); // Greater than or equal to 
+
+//Logical Operators:
+
+let a = true;
+let b = false;
+console.log(a && b); // Logical AND
+console.log(a || b); // Logical OR
+console.log(!a); // Logical NOT
+
+let marshmellow = false; 
+console.log (!marshmellow);
+
+
+
